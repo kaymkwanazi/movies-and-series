483af78a6c787e34480104101254097849f4b49f
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-08-07 15:53:44 +0200

    git commit -m "second"

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -7,12 +7,12 @@ export const Navbar = () => {
         <div className='bg-cover bg-center bg-no-repeat h-96' style={{ backgroundImage: `url(${onlyMurder})`}} >
             
             <nav className='flex items-center justify-between pr-12 pl-8 text-white'> 
-              <a href='/' ><span className='text-blue-500'>Enter-</span>Stream</a>
-                <ul className='list-none p-0 flex space-x-11 '>
+              <a href='/' className='text-xl'><span className='text-blue-500'>Enter-</span>Stream</a>
+                <ul className='list-none p-0 flex space-x-11 text-xs'>
                     <li><a href='/movies '>MOVIES</a></li>
                     <li><a href='/series '>SERIES</a></li>
                 </ul>
-                <button className='bg-purple-500 hover:bg-purple-700 font-bold rounded-full mt-8 py-2 px-4'>SUBSCRIBE</button>
+                <button className='bg-purple-500 hover:bg-purple-700 font-bold rounded-full mt-8 py-2 px-4 text-sm'>SUBSCRIBE</button>
             </nav>
         </div>
     </div>
